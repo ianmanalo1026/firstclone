cc47e50fd70ecc6937d62a404b5b2a7ea30d5ac3
Ian Manalo <69578172+ianmanalo1026@users.noreply.github.com>
2020-10-14 17:45:06 +0800

    Template updates

--- blog/urls.py
+++ blog/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     path('post/<int:blog_id>/edit/', views.PostUpdateView.as_view(), name='post_edit'),
     path('post/<int:blog_id>/remove/', views.PostDeleteView.as_view(), name='post_remove'),
     path('draft', views.DraftListView.as_view(), name='post_draft_list'),
+    path('post/<int:blog_id>/comment', views.add_comment_to_post, name='add_comment_to_post'),
+    path('comment/<int:blog_id>/approve', views.comment_approve, name='comment_approve'),
+    path('comment/<int:blog_id>/remove', views.comment_remove, name='comment_remove'),
+    path('post/<int:blog_id>/publish/', views.post_publish, name='post_publish'),
 ]
